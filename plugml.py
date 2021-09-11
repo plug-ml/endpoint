c820d77119701aab584850fd7873b008f3ba01ae
Gabriel Lee <gelos@localhost.localdomain>
2021-09-11 01:03:00 -0400

    Added basic library functions

---
+++ plugml.py
@@ -0,0 +1,30 @@
+import socket
+
+LOCALHOST = 'localhost'
+
+Connection = None
+
+class SourceNode:
+  def __init__(self, id):
+    self.id = id
+
+  def transmit(self, *value):
+    Connection.socket.sendall(','.join(list(map(lambda x: str(x).encode(), value))))
+
+class SinkNode:
+  def __init__(self, id):
+    self.id = id
+
+  def retrieve(self, *value):
+    data = ''
+    while (byte := Connection.socket.recv(1)) != b'\n':
+      data += byte.decode()
+    return list(map(float, data.split(',')))
+
+class __IPC:
+  def __init__(self, port):
+    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.socket.connect((LOCALHOST, port))
+
+def connect(port):
+  Connection = __IPC(port)
